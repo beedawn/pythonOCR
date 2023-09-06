fb656c5b9b6fbdf976e09fe8b45873489f3bcf4d
Bee Schmersal <69877469+bms0@users.noreply.github.com>
2023-09-06 19:59:58 -0400

    Update install_dependencies_mac.sh

--- installers/install_dependencies_mac.sh
+++ installers/install_dependencies_mac.sh
@@ -38,18 +38,19 @@ if [ "$(printf '%s\n' "3.10" "$python_version" | sort -V | head -n 1)" = "3.10" 
 else
     echo "Python version $python_version is below 3.10."
     echo "Installing Python 3.10.11..."
-    curl -o python-3.10.11-macos11.pkg "$pythonURL"
-    sudo installer -pkg python-3.10.11-macos11.pkg -target /
-    rm python-3.10.11-macos11.pkg
+    #curl -o python-3.10.11-macos11.pkg "$pythonURL"
+    #sudo installer -pkg python-3.10.11-macos11.pkg -target /
+    #rm python-3.10.11-macos11.pkg
+    brew install python@3.10
 fi
 
 
 # Install Python if not already installed
 if ! command -v python3 &> /dev/null; then
     echo "Installing Python..."
-    curl -o python-3.10.11-macos11.pkg "$pythonURL"
-    sudo installer -pkg python-3.10.11-macos11.pkg -target /
-    rm python-3.10.11-macos11.pkg
+    #curl -o python-3.10.11-macos11.pkg "$pythonURL"
+    #sudo installer -pkg python-3.10.11-macos11.pkg -target /
+    #rm python-3.10.11-macos11.pkg
 fi
 
 # Install Python packages from requirements.txt
